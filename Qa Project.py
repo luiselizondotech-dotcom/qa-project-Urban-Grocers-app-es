0cbaf19852c241407caf9790588a18e2c77d5454
Luis Elizondo <luis.elizondotech@gmail.com>
2025-10-01 12:57:26 -0700

    Update Qa Project.py

--- Qa Project.py
+++ Qa Project.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-68364b57-cff2-4bd3-804d-ccbd62a843dc.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-5ca62bb7-2b51-4392-ad25-c8df39579cf4.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
